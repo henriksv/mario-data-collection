b0601532b8c1b6b01ef00ea4618448ab57f0002e
henriksv <henrik.svo@gmail.com>
2020-01-22 14:31:04 +0100

    Cleanup

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# mario-data-collection
+# super mario data collection
 
 This is a method for playing in the gym-super-mario-bros environment as a human on a keyboard, while recording game-sessions and doing simultaneous video capture of the person playing.
 
-* '_image_viewer.py' is borrowed from the nes-py site-package.
+'_image_viewer.py' is borrowed from the nes-py site-package.
 
-* 'play_human_record.py' is a modified version of the method to play as human from the nes-py site-package
+'play_human_record.py' is a modified version of the method to play as human from the nes-py site-package
 
-
-Run 'play_and_record_video.sh' to start game and video capture
-
+run 'play_and_record_video.sh' to start game and video capture
--- video_capture.py
+++ video_capture.py
@@ -1,11 +1,9 @@
+"""A method to do video capture for a set number of frames from computer camera"""
+
 import cv2
 import time 
 import os
 import json
-
-# TODO:
-# Run capture for correct time
-# Determine folder structure
 
 
 video_capture_0 = cv2.VideoCapture(0)
@@ -14,11 +12,10 @@ video_capture_0 = cv2.VideoCapture(0)
 frame_width0 = int(video_capture_0.get(3))
 frame_height0 = int(video_capture_0.get(4))
 
-
+# create directory for storage if it does not exist
 video_path = './DATA/videos/'
 if not os.path.exists(video_path):
     os.makedirs(video_path)
-
 
 
 path, dirs, files = next(os.walk(video_path))
@@ -29,18 +26,19 @@ store_video_path = video_path + 'video' + str(file_count) + '.avi'
 out0 = cv2.VideoWriter(store_video_path, cv2.VideoWriter_fourcc(str('X'),str('V'),str('I'),str('D')), 30.0, (frame_width0,frame_height0))
 
 
-counter = 0
-#Sframes = 150 # 5sec
+#frames = 150 # 5sec
 #frames = 300 # 10sec
 #frames = 1800 # 1min
 #frames = 18000 # 10min
 #frames = 54000 # 30min
 frames = 63000 # 35min
 
+# extra frames to ensure full capture
 extra_frames = 30
-
 frames += extra_frames
 
+# start on frame 0
+counter = 0
 
 ret0, frame0 = video_capture_0.read()
 
@@ -50,23 +48,19 @@ while True:
     # Capture frame-by-frame
     ret0, frame0 = video_capture_0.read()
 
-
     if (ret0):
-        #write video
+        # write to video
         out0.write(frame0)
-        # show video
-        #cv2.imshow('Cam 0', frame0)
-
 
     if counter == frames:
         end_time = time.time()
         break
 
-
 # When everything is done, release the capture
 video_capture_0.release()
 out0.release()
 cv2.destroyAllWindows() # closes all frames
+
 
 timestamps = {}
 timestamps['start_time'] = start_time
@@ -77,14 +71,9 @@ path, dirs, files = next(os.walk(info_path))
 file_count = len(files)
 store_info_path = info_path + 'info' + str(file_count) + '.json'
 
-
 with open(store_info_path, 'w') as outfile:
         json.dump(timestamps, outfile)
 
 time_passed = end_time - start_time
-
-#print('Start time of video recording: ', start_time, '\n')
-#print('End time of video recording:   ', end_time, '\n')
-#print('Time passed : ', time_passed, '\n')
 
 print('Video capture finished')
