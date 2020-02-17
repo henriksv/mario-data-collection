56459340d00d39ffd15bf7a363a335fea212265f
henriksv <henrik.svo@gmail.com>
2020-02-17 11:44:28 +0100

    Adding DQN baseline

---
+++ mario_dqn/deep_DQN.py
@@ -0,0 +1,167 @@
+# URL: https://www.youtube.com/watch?v=5fHngyN8Qhw
+# Also: https://www.statworx.com/at/blog/using-reinforcement-learning-to-play-super-mario-bros-on-nes-using-tensorflow/
+
+from keras.layers import Dense, Activation, Conv2D, Flatten
+from keras.models import Sequential, load_model
+from keras.optimizers import Adam
+
+import numpy as np
+
+class ReplayBuffer(object):
+    def __init__(self, max_size, input_shape, n_actions, discrete=False):
+        self.mem_size = max_size
+        self.discrete = discrete
+        self.state_memory = np.zeros((self.mem_size, *input_shape))
+        self.new_state_memory = np.zeros((self.mem_size, *input_shape))
+        dtype = np.int8 if self.discrete else np.float32
+        self.action_memory = np.zeros((self.mem_size, n_actions), dtype=dtype)
+        self.reward_memory = np.zeros(self.mem_size)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.float32)
+        self.mem_cntr = 0
+
+    def store_transition(self, state, action, reward, state_, done):
+        index = self.mem_cntr % self.mem_size
+        self.state_memory[index] = state
+        self.new_state_memory[index] = state_
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = 1 - int(done)
+        if self.discrete:
+            actions = np.zeros(self.action_memory.shape[1])
+            actions[action] = 1.0
+            self.action_memory[index] = actions
+        else:
+            self.action_memory[index] = action
+        self.mem_cntr += 1
+
+    def sample_buffer(self, batch_size):
+        max_mem = min(self.mem_cntr, self.mem_size)
+        batch = np.random.choice(max_mem, batch_size)
+
+        states = self.state_memory[batch]
+        states_ = self.new_state_memory[batch]
+        rewards = self.reward_memory[batch]
+        actions = self.action_memory[batch]
+        terminal = self.terminal_memory[batch]
+
+
+        #print('actions', actions.shape)
+
+        return states, actions, rewards, states_, terminal
+
+def build_dqn_old(lr, n_actions, input_dims, fc1_dims, fc2_dims):
+
+    model = Sequential([
+        Dense(fc1_dims, input_shape=(input_dims, )),
+        Activation('relu'),
+        Dense(fc2_dims),
+        Activation('relu'),
+        Dense(n_actions)
+    ])
+
+    model.compile(optimizer=Adam(lr=lr), loss='mse')
+
+    return model
+
+def build_dqn(lr, n_actions, input_dims):
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=8, strides=4, activation='relu',
+                     input_shape=(*input_dims,)))
+
+    model.add(Conv2D(filters=32, kernel_size=4, strides=2, activation='relu'))
+    model.add(Conv2D(filters=32, kernel_size=3, strides=1, activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(n_actions))
+
+    model.compile(optimizer=Adam(lr=lr), loss='mean_squared_error')
+
+    return model
+
+class Agent(object):
+    def __init__(self, alpha, gamma, n_actions, epsilon, batch_size, input_dims, replace, 
+                epsilon_dec=0.996, epsilon_end=0.01, mem_size=1000000, 
+                q_eval_fname='q_eval.h5', q_target_fname='q_target.h5'):
+        
+        self.action_space = [i for i in range(n_actions)]
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_dec = epsilon_dec
+        self.epsilon_min = epsilon_end
+        self.batch_size = batch_size
+        self.replace = replace
+        self.learn_step = 0
+        self.q_target_model_file = q_target_fname
+        self.q_eval_model_file = q_eval_fname
+
+        self.train_every = 3 # Train on every .th step
+
+        self.memory = ReplayBuffer(mem_size, input_dims, n_actions, discrete=True)
+        self.q_eval = build_dqn(alpha, n_actions, input_dims)#, 256, 256)
+        self.q_next = build_dqn(alpha, n_actions, input_dims)#, 256, 256)
+
+    def replace_target_network(self):
+        if self.replace is not None and self.learn_step % self.replace == 0:
+            self.q_next.set_weights(self.q_eval.get_weights())
+
+    def remember(self, state, action, reward, new_state, done):
+        self.memory.store_transition(state, action, reward, new_state, done)
+
+    def choose_action(self, state):
+        state = state[np.newaxis, :]
+        rand = np.random.random()
+        if rand < self.epsilon:
+            action = np.random.choice(self.action_space)
+        else:
+            actions = self.q_eval.predict(state)
+            action = np.argmax(actions)
+
+        return action
+
+    def learn(self):
+        if  self.memory.mem_cntr < self.batch_size:
+            return
+        
+        # Train on every
+        if self.memory.mem_cntr == 0 or self.memory.mem_cntr % self.train_every != 0:
+            return
+
+        state, action, reward, new_state, done = self.memory.sample_buffer(self.batch_size)
+
+        self.replace_target_network()
+
+        q_eval = self.q_eval.predict(state)
+        q_next = self.q_next.predict(new_state)
+
+
+        # Revert back from one-hot encoding
+        action_values = np.array(self.action_space, dtype=np.int8)
+        action_indices = np.dot(action, action_values)
+
+        q_target = q_eval[:]
+        #q_target = q_eval.copy()
+
+        batch_index = np.arange(self.batch_size, dtype=np.int32)
+
+        q_target[batch_index, action_indices] = \
+            reward + self.gamma*np.max(q_next, axis=1)*done
+
+        _= self.q_eval.fit(state, q_target, verbose=0)
+        # self.q_eval.train_on_batch(state, q_target)
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon = self.epsilon*self.epsilon_dec
+        else:
+            self.epsilon = self.epsilon_min
+        
+        self.learn_step += 1
+
+
+    def save_models(self):
+        self.q_eval.save(self.q_eval_model_file)
+        self.q_next.save(self.q_target_model_file)
+        print('... saving models ...')
+
+    def load_models(self):
+        self.q_eval = load_model(self.q_eval_model_file)
+        self.q_nexdt = load_model(self.q_target_model_file)
+        print('... loading models ...')
---
+++ mario_dqn/main_SMB.py
@@ -0,0 +1,92 @@
+#from deepQ_keras import Agent
+from deep_DQN import Agent
+import numpy as np
+from utils import plotLearning
+import gym
+from nes_py.wrappers import JoypadSpace
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT, RIGHT_ONLY
+from wrappers import wrapper
+
+import cv2
+from collections import deque
+
+
+if __name__=='__main__':
+    #env = gym.make('CartPole-v0')
+    env = gym_super_mario_bros.make('SuperMarioBros-v0')
+    env = JoypadSpace(env, SIMPLE_MOVEMENT)
+    env = wrapper(env)
+
+   # fname_base = 'mario_DDQN-20_60-64_32-8-4_32-4-2_32-3-1_512'
+    fname_base = 'mario_DDQN_test4'
+    n_games = 10000
+
+    #dims = (240, 256, 3)
+    dims = (84, 84, 4)
+    n_frames = 4
+    img_rows, img_cols = dims[0], dims[1] #Downscaled
+
+    fname_eval = fname_base + '_eval.h5'
+    fname_next = fname_base + '_next.h5'
+
+    agent = Agent(gamma=0.9, epsilon=1.0, alpha=0.00025, input_dims=dims, n_actions=7, mem_size=7000, 
+                    batch_size=32, epsilon_end=0.01, replace=50, 
+                    q_eval_fname=fname_eval, q_target_fname=fname_next)
+
+    #agent.load_model()
+
+    scores = []
+    eps_history = []
+
+    #for i in range(n_games):
+
+    i = 0
+    while(True):
+        done = False
+        score = 0
+        observation = env.reset()
+        #print(observation.shape)
+        #observation = cv2.resize(observation, dsize=(img_cols, img_rows), interpolation=cv2.INTER_CUBIC)
+        #print(observation.shape)
+
+        #nstep = 0
+
+        while not done:# and nstep < 100:
+            #print('step: ', nstep)
+            #nstep+=1
+
+            action = agent.choose_action(observation)
+            observation_, reward, done, info = env.step(action)
+
+            #observation_ = cv2.resize(observation_, dsize=(img_cols, img_rows), interpolation=cv2.INTER_CUBIC)
+
+            score += reward
+            agent.remember(observation, action, reward, observation_, done)
+
+            env.render()
+            observation = observation_
+            agent.learn()
+
+        eps_history.append(agent.epsilon)
+        scores.append(score)
+
+        avg_score = np.mean(scores[max(0,i-100):(i+1)])
+        print('episode ', i, 'score %.2f' % score, ' average score %.2f' % avg_score)
+
+        if i % 100 == 0 and i > 0:
+            #agent.save_model(fname_base + '.h5')
+            
+            
+            agent.save_models()
+        
+        if i % 1000 == 0 and i > 0:
+
+            # for plotLearning
+            #filename = fname_base + '_ep' + str(i+1) + '.png'
+            filename = fname_base + '.png'
+            
+            x = [j+1 for j in range(i+1)]
+            plotLearning(x, scores, eps_history, filename)
+
+        i +=1
