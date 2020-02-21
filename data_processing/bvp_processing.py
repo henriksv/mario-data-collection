b31e18541c90b1d27153e8bf01b903f8016038a8
Henrik <henrik.svo@gmail.com>
2020-02-21 11:03:04 +0100

    Some cleaning

--- data_processing/bvp_processing.py
+++ data_processing/bvp_processing.py
@@ -64,9 +64,9 @@ for i in range(j,k):
 
 max_y = max(y)
 
-#min_y = min(y) # Normalize with actual min
+min_y = min(y) # Normalize with actual min
 
-min_y = -max_y # Normalize with reverse of max as min
+#min_y = -max_y # Normalize with reverse of max as min
 
 
 range_y = max_y - min_y
@@ -121,8 +121,8 @@ for i in range(0, len(normalized_y)):
         for j in range(start, i+1):
             single_peak_values.append(cur_val)
 
-print('Len of spv:', len(single_peak_values))
-print('Len of ny:', len(normalized_y))
+#print('Len of spv:', len(single_peak_values))
+#print('Len of ny:', len(normalized_y))
         
 cur_val = 0.5
 start = 0
@@ -141,26 +141,7 @@ for i in range(0, len(normalized_y)):
             single_peak_values2.append(cur_val)
 
 
-print('Len of spv2:', len(single_peak_values2))
-"""
-for i in range(0, len(normalized_y)):
-    if i in peaks:
-        new_val = normalized_y[i]
-        if new_val < 0:
-            cur_val = 0
-        else:
-            cur_val = normalized_y[i]
-    single_peak_values.append(cur_val)
-
-cur_val = 0.5
-
-for i in range(0, len(normalized_y)):
-    if i in peaks:
-        new_val = normalized_y[i]
-        if new_val > 0.1:
-            cur_val = normalized_y[i]
-    single_peak_values2.append(cur_val)
-"""
+#print('Len of spv2:', len(single_peak_values2))
 
 normalized_y_positive = []
 
@@ -207,101 +188,6 @@ plt.show()
 
 #https://matplotlib.org/3.1.1/gallery/text_labels_and_annotations/annotation_demo.html
 
-"""
-i = 0
-last_peak = None
-last_valley = None
-rising = None
 
-skip_start = 0
-skip_end = 0
-
-last = len(normalized_y)-1
-vasoconstriction = 0
-
-while i < last-1:
-    if i == 0: 
-        # Case of falling start
-        if normalized_y[i] > normalized_y[i+1]:
-            while normalized_y[i] > normalized_y[i+1]:
-                i+=1
-                e4_start += timestep
-                skip_start += 1
-            last_valley = normalized_y[i]
-            rising = True
-            
-
-        # Case of rising start
-        else:
-            while normalized_y[i] <= normalized_y[i+1]:
-                i+=1
-                e4_start += timestep
-                skip_start += 1
-            last_peak = normalized_y[i]
-            rising = False
-    else:
-        if rising:
-            peak = i
-            valley = i
-
-            while normalized_y[peak] < normalized_y[peak+1]:
-                if peak >= last-1:
-                    print('last')
-                    skip_end = last - valley
-                    i = last
-                    single_peak_values.append(vasoconstriction)
-                    break
-
-                peak+=1
-
-
-            valley_val = normalized_y[valley]
-            peak_val = normalized_y[peak]
-
-            vasoconstriction = peak_val - valley_val
-            last_peak = peak
-            i = peak
-            rising = False
-
-            for j in range(valley, peak):
-                single_peak_values.append(vasoconstriction)
-        else:
-            peak = i
-            valley = i
-
-            while normalized_y[valley] >= normalized_y[valley+1]:
-                if valley >= last-1:
-                    print('last')
-                    skip_end = last - peak
-                    i = last
-                    single_peak_values.append(vasoconstriction)
-                    break
-                valley+=1
-
-            valley_val = normalized_y[valley]
-            peak_val = normalized_y[peak]
-
-            vasoconstriction = peak_val - valley_val
-            last_valley = valley
-            i = valley
-            rising = True
-            
-            for j in range(peak, valley):
-                single_peak_values.append(vasoconstriction)
-
-print(skip_start)
-print(skip_end)
-    
-print(len(single_peak_values))
-
-y_cropped = len(normalized_y) - skip_end - skip_start
-print(y_cropped)
-
-
-for i in range(0, 200):
-    print(normalized_y[last-i])
-    #print(single_peak_values[i])
-
-"""
 
 #x2 = range(0, len(normalized_y))
