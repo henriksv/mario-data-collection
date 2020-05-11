313952f3928b4c5686edce7d58feaddb886fb188
Henrik Svoren <henriksv@g001.cm.cluster>
2020-05-11 11:44:14 +0200

    New unfinished synch method

--- data_processing/synch_e4_data_with_game_frames.py
+++ data_processing/synch_e4_data_with_game_frames.py
@@ -2,7 +2,7 @@ import json
 import csv
 
 
-def synch_e4(e4_type, session_num):
+def synch_e4_by_pruning(e4_type, session_num):
     session_number = str(session_num)
     session_path = './DATA/sessions/session' + session_number
     with open(session_path) as json_file:
@@ -97,6 +97,59 @@ def synch_e4(e4_type, session_num):
     with open(out_file_path, "w") as f:
         for value in matching_e4:
             f.write("%s\n" % value)
+
+def synch_e4_with_padded(e4_type, session_num):
+    session_number = str(session_num)
+    session_path = './DATA/sessions/session' + session_number
+    with open(session_path) as json_file:
+        session = json.load(json_file)
+
+    #
+    gap_info_path = './DATA/gap_info/gap_info'+session_number
+    with open(gap_info_path) as json_file:
+        gap_info = json.load(json_file)
+
+    e4_path = './DATA/e4/'+session_number+'/'+e4_type+'.csv'
+    e4 = []
+    with open(e4_path, newline='') as csvfile:
+        rd = csv.reader(csvfile)
+        for row in rd:
+            e4.append(row)
+
+    e4_pr_sec = float(e4[1][0])
+    e4_start = float(e4[0][0])
+
+    session_start = session['start_time']
+    session_stop = session['stop_time']
+
+
+    n_frames = len(session['obs'])
+    missing = gap_info['missing']
+    gap_indices = gap_info['indices']
+    n_gaps = len(gap_indices)
+    avg_gap = int(missing/n_gaps)
+    extra = missing % n_gaps
+    frame_pr_sek = 60
+    
+    timestep = 1 / e4_pr_sec
+    j = 2
+
+    while e4_start < session_start-timestep/2:
+        e4_start += timestep
+        j +=1
+
+    k = j
+    e4_end = e4_start
+
+    while e4_end < session_stop-timestep/2:
+        e4_end += timestep
+        k +=1
+
+
+    synched_e4 = []
+    for i in range(j,k):
+        synched_e4.append(e4[i])
+        
 
 if __name__ == "__main__":
     synch_e4("EDA", 6)
