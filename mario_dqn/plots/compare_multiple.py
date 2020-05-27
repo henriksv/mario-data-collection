4c4529fdf8e2c00642dfed301c12d17aae399499
Henrik Svoren <henriksv@g001.cm.cluster>
2020-05-27 13:00:21 +0200

    More plots

--- mario_dqn/plots/compare_multiple.py
+++ mario_dqn/plots/compare_multiple.py
@@ -3,91 +3,127 @@ import numpy as np
 import gym
 import json
 
+def compare(paths, legends, metric, label, test, limit=10000, avg_n=500):
+
+    out_path = 'CompareMult_'+metric+'_'+str(limit)+'_'+test+'.png'
+    if avg_n != 500:    
+        out_path = 'CompareMult_avg' + str(avg_n )+'_'+ metric+'_'+str(limit)+'_'+test+'.png'
+    
+
+    data = []
+    for p in paths:
+        with open(p) as json_file:
+            data.append(json.load(json_file))
+
+    vals = []
+    for d in data:
+        vals.append(d[metric][0:limit])
+
+    i = 0
+    x = [i+1 for i in range(0, limit)]#len(vals))]
+    if metric == 'scores':   
+        x = [i+1 for i in range(avg_n, limit)]#len(vals))]
+
+
+    fig=plt.figure()
+    ax=fig.add_subplot(111, label="1")
+
+    avgs = []
+
+    for v in vals:
+        N = len(v)
+        running_avg = np.empty(N)
+        for t in range(N):
+            running_avg[t] = np.mean(v[max(0, t-avg_n):(t+1)])
+        #For score
+        if metric == 'scores':
+            running_avg = running_avg[avg_n:]
+        avgs.append(running_avg)
+
+    for i, avg in enumerate(avgs):
+        color = 'C' + str(i+1)
+        ax.plot(x, avg, color=color, label=legends[i])
+
+
+    ax.set_xlabel("Episode", color="C0")
+    ax.set_ylabel(label, color="C0")
+    ax.tick_params(axis='x', colors="C0")
+    ax.tick_params(axis='y', colors="C0")
+
+    if len(paths) == 3:
+        ax.legend(loc='lower right')#, frameon=False)
+    else:
+        ax.legend(loc='upper left')#, frameon=False)
+
+
+    plt.savefig(out_path)
+    plt.close()
+
+
+
 # Orange, green, red, purple
 
 #m100k_lr0.00001_ed0.99995 - 30k -running
 path1 = '/work/henriksv/code/mario-data-collection/mario_dqn/mario_DDQN_m100k_lr0.00001_ed0.99995-2/mario_DDQN_m100k_lr0.00001_ed0.99995-2_episodes.json'
-
 #m100k_lr0.00005_ed0.99995 - 40k
 path2 = '/work/henriksv/code/mario-data-collection/mario_dqn/mario_DDQN_m100k_lr0.00005_ed0.99995-2/mario_DDQN_m100k_lr0.00005_ed0.99995-2_episodes.json'
-
 #m100k_lr0.0001_ed0.99995 - 40k
 path3 = '/work/henriksv/code/mario-data-collection/mario_dqn/mario_DDQN_m100k_lr0.0001_ed0.99995-2/mario_DDQN_m100k_lr0.0001_ed0.99995-2_episodes.json'
-
 #m100k_lr0.00025_ed0.99995 - 18k
 path4 = '/work/henriksv/code/mario-data-collection/mario_dqn/mario_DDQN_m100k_ed0.99995-2/mario_DDQN_m100k_ed0.99995-2_episodes.json'
-
 #m100k_lr0.001_ed0.99995 - 14k
 path5 = '/work/henriksv/code/mario-data-collection/mario_dqn/mario_DDQN_m100k_lr0.001_ed0.99995/mario_DDQN_m100k_lr0.001_ed0.99995_episodes.json'
 
 paths = [path1, path2, path3, path4, path5]
-#paths = [path1, path2, path3]
-
 legends = ['Agent 1: LR:0.00001', 'Agent 2: LR:0.00005', 'Agent 3: LR:0.0001', 'Agent 4: LR:0.00025', 'Agent 5: LR:0.001']
-#legends = ['LR:0.00001', 'LR:0.00005', 'LR:0.0001']
-
-
-
-#path1 = '/work/henriksv/code/mario-data-collection/mario_dqn/mario_DDQN_m100k_ed0.99995-2/mario_DDQN_m100k_ed0.99995-2_episodes.json'
-#path2 = '/work/henriksv/code/mario-data-collection/mario_dqn/mario_DDQN_m100k_ed0.99995-1/mario_DDQN_m100k_ed0.99995-1_episodes.json'
-#path2 = '/work/henriksv/code/mario-data-collection/mario_dqn/mario_DDQN_testingDQN_m50k_ed0.99995/mario_DDQN_testingDQN_m50k_ed0.99995_episodes.json'
-
-#paths = [path1, path2]#, path5, path4, path5]
-
-limit = 40000
-avg_n=500
-metric = 'success'
 label = 'Finish Rate (pr. 500 eps)'
-out_path = 'CompareMult_'+metric+'_10k_lr.png'
+test = 'lr'
+compare(paths, legends, 'success', label,test, 10000)
+label = 'Finish Rate (pr. 1000 eps)'
+compare(paths, legends, 'success', label,test, 10000, 1000)
 
-data = []
-for p in paths:
-    with open(p) as json_file:
-        data.append(json.load(json_file))
+label = 'Average Score (pr. 500 eps)'
+compare(paths, legends, 'scores', label,test, 10000)
+label = 'Average Score (pr. 1000 eps)'
+compare(paths, legends, 'scores', label,test, 10000, 1000)
 
-vals = []
-for d in data:
-    vals.append(d[metric][0:limit])
+paths = [path1, path2, path3]
+legends = ['Agent 1: LR:0.00001', 'Agent 2: LR:0.00005', 'Agent 3: LR:0.0001', 'Agent 4: LR:0.00025', 'Agent 5: LR:0.001']
+label = 'Finish Rate (pr. 500 eps)'
+compare(paths, legends, 'success', label,test, 40000)
+label = 'Finish Rate (pr. 1000 eps)'
+compare(paths, legends, 'success', label,test, 40000, 1000)
 
-i = 0
-x = [i+1 for i in range(0, limit)]#len(vals))]
-if metric == 'scores':   
-    x = [i+1 for i in range(avg_n, limit)]#len(vals))]
+label = 'Average Score (pr. 500 eps)'
+compare(paths, legends, 'scores', label,test, 40000)
+label = 'Average Score (pr. 1000 eps)'
+compare(paths, legends, 'scores', label,test, 40000, 1000)
 
+#Compare memory
 
-fig=plt.figure()
-ax=fig.add_subplot(111, label="1")
+#m50k_lr0.00005
+path6 = '/work/henriksv/code/mario-data-collection/mario_dqn/mario_DDQN_m50k_lr0.00005/mario_DDQN_m50k_lr0.00005_episodes.json'
+#m250k_lr0.00005
+path7 = '/work/henriksv/code/mario-data-collection/mario_dqn/mario_DDQN_m250k_lr0.00005/mario_DDQN_m250k_lr0.00005_episodes.json'
+#m500k
+path8 = '/work/henriksv/code/mario-data-collection/mario_dqn/mario_DDQN_m500k_lr0.00005/mario_DDQN_m500k_lr0.00005_episodes.json'
+test = 'mem'
+paths = [path6, path2, path7, path8]
+legends = ['Agent 1: Mem:50k', 'Agent 2: Mem:100k', 'Agent 3: Mem:250k', 'Agent 4: Mem:500k']
 
-avgs = []
+label = 'Finish Rate (pr. 500 eps)'
+compare(paths, legends, 'success', label,test, 17000)
+label = 'Finish Rate (pr. 1000 eps)'
+compare(paths, legends, 'success', label,test, 17000, 1000)
 
-for v in vals:
-    N = len(v)
-    running_avg = np.empty(N)
-    for t in range(N):
-        running_avg[t] = np.mean(v[max(0, t-avg_n):(t+1)])
-    #For score
-    if metric == 'scores':
-        running_avg = running_avg[avg_n:]
-    avgs.append(running_avg)
-
-for i, avg in enumerate(avgs):
-    color = 'C' + str(i+1)
-    ax.plot(x, avg, color=color, label=legends[i])
-
-
-ax.set_xlabel("Episode", color="C0")
-ax.set_ylabel(label, color="C0")
-ax.tick_params(axis='x', colors="C0")
-ax.tick_params(axis='y', colors="C0")
-
-if len(paths) == 3:
-    ax.legend(loc='lower right')#, frameon=False)
-else:
-    ax.legend(loc='upper left')#, frameon=False)
+label = 'Average Score (pr. 500 eps)'
+compare(paths, legends, 'scores', label,test, 17000)
+label = 'Average Score (pr. 1000 eps)'
+compare(paths, legends, 'scores', label,test, 17000, 1000)
 
 
-plt.savefig(out_path)
-plt.close()
+
+
 
 """
    episode_history = {
